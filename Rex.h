190c413afb22428cf02dcf447f46360d87876a17
Mr. Rc <60568652+HACKE-RC@users.noreply.github.com>
2023-05-20 20:29:41 +0530

    Added function signatures and data types for updated Rex.

--- Rex.h
+++ Rex.h
@@ -6,7 +6,9 @@
 #include <vector>
 #include <fstream>
 #include <map>
-
+#include <algorithm>
+#include <cctype>
+#include <filesystem>
 
 typedef std::vector<std::string> VMCODE;
 typedef std::vector<std::string> ASSEMBLY;
@@ -20,7 +22,12 @@ private:
 
    enum insType {
        MONOINS = 1,
-       DUOINS
+       DUOINS,
+       CONDITIONAL_BRANCH,
+       UNCONDITIONAL_BRANCH,
+       CALL,
+       LABEL,
+       ERROR_INVALID_INSTRUCTION = -7,
    };
 
    enum mnemonicType{
@@ -34,14 +41,27 @@ private:
        MNEMONIC_LT,
        MNEMONIC_GT,
        MNEMONIC_NEG,
-       MNEMONIC_EQ
+       MNEMONIC_EQ,
+       MNEMONIC_CALL,
+       MNEMONIC_IF_GO_TO,
+       MNEMONIC_GO_TO,
+       MNEMONIC_RETURN,
+       MNEMONIC_LABEL,
+       MNEMONIC_FUNCTION
    };
 
-    std::map<std::string, mnemonicType> m_strMnemonicType = {{"push", MNEMONIC_PUSH}, {"pop", MNEMONIC_POP}, {"add", MNEMONIC_ADD}, {"sub", MNEMONIC_SUB}, {"and", MNEMONIC_AND}, {"or", MNEMONIC_OR}, {"not", MNEMONIC_NOT}, {"lt", MNEMONIC_LT}, {"gt", MNEMONIC_GT}, {"neg", MNEMONIC_NEG}, {"eq", MNEMONIC_EQ}};
+    std::map<std::string, mnemonicType> m_strMnemonicType = {{"push", MNEMONIC_PUSH}, {"pop", MNEMONIC_POP}, {"add", MNEMONIC_ADD},
+                                                             {"sub", MNEMONIC_SUB}, {"and", MNEMONIC_AND},{"or", MNEMONIC_OR},
+                                                             {"not", MNEMONIC_NOT}, {"lt", MNEMONIC_LT}, {"gt", MNEMONIC_GT},
+                                                             {"neg", MNEMONIC_NEG}, {"eq", MNEMONIC_EQ}, {"if-goto", MNEMONIC_IF_GO_TO},
+                                                             {"goto", MNEMONIC_GO_TO}, {"return", MNEMONIC_RETURN}, {"call", MNEMONIC_CALL},
+                                                             {"label", MNEMONIC_LABEL}, {"function", MNEMONIC_FUNCTION}};
 
 public:
    struct instructionInfo{
-            int insType;
+            insType instructionType;
+            std::string functionName;
+            std::string nArgs;
 
             std::string mnemonic;
             std::string segment;
@@ -50,6 +70,7 @@ public:
     };
 
 private:
+    int m_routine = 0;
     std::map<std::string, std::string> m_segAddr = {{"local", "1"}, {"argument", "2"},
                                                     {"this", "3"}, {"that", "4"},
                                                     {"temp", "5"}, {"pointer", "3"}};
@@ -59,11 +80,10 @@ private:
 public:
     enum statusCode {
         ERROR_FILE_NOT_FOUND = -1,
-        ERROR_INVALID_VMCODE = -7,
     };
 
-    VMCODE validMonoIns = {"add", "sub", "eq", "lt", "gt", "not", "or", "and", "neg"};
-    VMCODE validDuoIns = {"push", "pop"};
+    VMCODE validMonoIns = {"add", "sub", "eq", "lt", "gt", "not", "or", "and", "neg", "return"};
+    VMCODE validDuoIns = {"push", "pop", "label", "if-goto", "goto", "call", "function"};
     VMCODE validSegments = {"constant", "local", "argument", "this", "that", "temp", "pointer"};
 
 
@@ -72,13 +92,29 @@ public:
         void cleanVMCode();
         ASSEMBLY parseVMCode();
         void printVMCode();
-        statusCode writeOutput();
+        void writeOutput();
 
 private:
-    instructionInfo getVMInsInfo(std::string& code);
+    instructionInfo getVMInsInfo(std::string& code, ASSEMBLY& assembly);
     static bool isValid(VMCODE const& vec, std::string& str);
     static bool isNumber(std::string& str);
     mnemonicType identifyInstruction(std::string& str);
-};
+    static void to_lower(std::string& str);
 
+
+private:
+    void translatePush(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translatePop(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateArithmetic(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateEquals(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateComparison(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateBinaryOp(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateBinaryOp2(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateCall(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateLabel(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateGoTo(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateReturn(instructionInfo insInfo, ASSEMBLY& assembly);
+    void translateFunction(instructionInfo insInfo, ASSEMBLY& assembly);
+
+};
 #endif //VM_TRANSLATOR_REX_H
